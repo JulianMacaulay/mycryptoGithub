7a91be050c91ae340960fe2727e6ad12c25949bf
julian <879025591@qq.com>
2025-11-18 13:10:54 +0800

    在init中增加了对传统z-score计算和arima-garchd的z-score计算策略的引用

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -5,6 +5,15 @@
 
 from .base_strategy import BaseStrategy
 from .turtle_strategy import TurtleStrategy
+from .base_zscore_strategy import BaseZScoreStrategy
+from .traditional_zscore_strategy import TraditionalZScoreStrategy
+from .arima_garch_zscore_strategy import ArimaGarchZScoreStrategy
 
-__all__ = ['BaseStrategy', 'TurtleStrategy']
+__all__ = [
+    'BaseStrategy',
+    'TurtleStrategy',
+    'BaseZScoreStrategy',
+    'TraditionalZScoreStrategy',
+    'ArimaGarchZScoreStrategy'
+]
 
